dd10b9c5a389855e92ecf3454c3c63d77ed069b5
trevorling <53426191+trevorling@users.noreply.github.com>
2026-06-25 13:09:05 +0300

    feat(6): private & public applications

--- api/src/public-notifications/controllers/public-notifications.controller.ts
+++ api/src/public-notifications/controllers/public-notifications.controller.ts
@@ -1,22 +1,8 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  MessageEvent,
-  Post,
-  Query,
-  Sse,
-} from '@nestjs/common';
-import {
-  ApiAcceptedResponse,
-  ApiBadRequestResponse,
-  ApiOkResponse,
-  ApiOperation,
-} from '@nestjs/swagger';
+import { Controller, MessageEvent, Query, Sse } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
 
-import { CreateNotificationEventBodyDto, NotificationEventsQueryDto } from '../../notification/dtos';
+import { NotificationEventsQueryDto } from '../../notification/dtos';
 import { NotificationService } from '../../notification/services';
 
 @Controller({ version: '1', path: '/notifications' })
@@ -33,20 +19,5 @@ export class PublicNotificationsController {
   })
   public subscribeToEvents(@Query() query: NotificationEventsQueryDto): Promise<Observable<MessageEvent>> {
     return this.notificationService.getEventSse(query);
-  }
-
-  @Post('/events')
-  @HttpCode(HttpStatus.ACCEPTED)
-  @ApiOperation({ summary: 'Publish a notification event' })
-  @ApiAcceptedResponse({
-    description: 'Notification event was accepted by RabbitMQ',
-  })
-  @ApiBadRequestResponse({
-    description: 'Invalid notification event envelope',
-  })
-  public async publishNotificationEvent(
-    @Body() body: CreateNotificationEventBodyDto,
-  ): Promise<void> {
-    await this.notificationService.publishNotificationEvent(body);
   }
 }
